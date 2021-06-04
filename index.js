c693821835467dc0d5f73a7c55923427fe414981
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-04 17:14:18 -0300

    instalação do pacote nodemon para o desenvolvimento do projeto

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 const PORT = 3000
 var listener = app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`))
 
-app.get('/atendimento', (req, res) => res.send("Você está na rota de atendimento"))
+app.get('/atendimento', (req, res) => res.send("Você está na sessão de atendimento"))
