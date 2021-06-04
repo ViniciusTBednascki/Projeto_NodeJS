f5fba6818a35179d3f4f10e78f74c622c7317410
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-04 19:00:11 -0300

    criando um handler para o POST no recurso atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
     app.get('/atendimento', (req, res) => res.send("Você está na rota de atendimento e está realizando uma requisição GET"))
+
+    app.post('/atendimento', (req, res) => {
+        console.log(req.body)    
+        res.send("Você está na rota de atendimento e está realizando uma requisição POST")
+    })
 }
