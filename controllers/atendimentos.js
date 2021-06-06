2294b9db49651a035d96abe6061df8931fe37f88
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-06 19:18:09 -0300

    adicionado data marcada e de criação à tabela

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.get('/atendimento', (req, res) => res.send("Você está na rota de atendimento e está realizando uma requisição GET"))
 
     app.post('/atendimento', (req, res) => {
-        console.log('Requisição POST realizada')
+        console.log('resolvendo requisição POST')
 
         const atendimento = req.body
         Atendimento.adicionar(atendimento)
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
 const conexao =require('../infraestrutura/conexao')
+const moment = require('moment')
 
 class Atendimento {
     adicionar(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultado) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultado) => {
             if(erro){
                 console.log(erro)
             }else {
