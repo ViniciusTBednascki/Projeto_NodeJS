91ccabbe88fe8ee3777a7c494716327794f2a6b2
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-06 18:36:21 -0300

    utilizando o modelo para inserir os dados na tabela

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimento', (req, res) => res.send("Você está na rota de atendimento e está realizando uma requisição GET"))
 
     app.post('/atendimento', (req, res) => {
-        console.log(req.body)    
+        console.log('Requisição POST realizada')
+
+        const atendimento = req.body
+        Atendimento.adicionar(atendimento)
+
         res.send("Você está na rota de atendimento e está realizando uma requisição POST")
     })
 }
