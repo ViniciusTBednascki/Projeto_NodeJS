3af2672b98e6ccfd913f438cb1a453593d14b30c
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-06 18:17:21 -0300

    conexão com o banco de dados e criação das tabelas

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const customExpress = require('./config/customExpress')
-const app = customExpress()
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const PORT = 3000
-var listener = app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`))
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log("conectado ao banco de dados com sucesso")
+    
+        Tabelas.init(conexao)
+        const app = customExpress()
+
+        const PORT = 3000
+        app.listen(PORT, () => console.log(`Escutando na porta ${PORT}`))
+    }
+})
